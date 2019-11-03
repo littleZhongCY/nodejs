fa36c189bd442a0dd9b5f69a5810d991b2c001c7
zhong <zhong>
2019-11-03 19:07:55 +0800

    test

--- routes/news.js
+++ routes/news.js
@@ -3,9 +3,11 @@ var router = express.Router()
 let mongoose = require('mongoose')
 
 mongoose.connect(
-  'mongodb://47.101.134.233:27017/user',
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function(err) {
+  'mongodb://47.101.134.233:27017/user', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  function (err) {
     if (err) throw err
     console.log('数据库连接成功')
   }
@@ -16,16 +18,30 @@ let newsSchema = new mongoose.Schema({
 
 let newModle = mongoose.model('news', newsSchema, 'news')
 /* GET users listing. */
-router.get('/getNews', function(req, res, next) {
+router.get('/getNews', function (req, res, next) {
   newModle.find({}).exec((err, data) => {
     if (err) throw err
     res.send({
-      data: data,
+      data: data.reverse(),
       code: 0
     })
   })
 })
-router.post('/uploadNews', function(req, res, next) {
+
+router.post('/changeNews', function (req, res) {
+  console.log(req.body)
+  newModle.findById(req.body.id).exec((err, data) => {
+    data.content = req.body.content
+    data.save()
+    res.send({
+      data: '修改成功',
+      code: 0
+    })
+  })
+})
+
+
+router.post('/uploadNews', function (req, res, next) {
   if (req.body.content) {
     let newsEntity = new newModle()
     newsEntity.content = req.body.content
@@ -41,12 +57,24 @@ router.post('/uploadNews', function(req, res, next) {
     })
   }
 })
-router.post('/removeNews', function(req, res, next) {
-  newModle.find({ content: req.query.content }).exec((err, data) => {
-    if (err) throw err
-    data[0].remove(function(err) {
+router.post('/removeNews', function (req, res, next) {
+  console.log(req.body)
+  newModle.findById(
+    req.body.content
+  ).exec((err, data) => {
+    if (!data) {
+      res.send({
+        data: '删除失败,数据不存在',
+        code: -1
+      })
+      return
+    }
+    data.remove(function (err) {
       if (err) throw err
-      console.log('删除成功')
+      res.send({
+        data: '删除成功',
+        code: 0
+      })
     })
   })
 })
